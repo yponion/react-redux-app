0ce9bbe5c648aa00a47c7721171427b478f25391
yponion <yp071704@naver.com>
2024-09-13 20:16:14 +0900

    fetch: end of part3-ch2-8
     - redux
       redux-thunk 등 레거시 한 버전
       현재는 redux-toolkit이 권장됨

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { applyMiddleware, createStore } from "redux"; // redux toolkit이 권장되지만 일단 이걸로 실습하고 바꾼다함
+import rootReducer from "./reducers";
+import { Provider } from "react-redux";
+import { thunk } from "redux-thunk";
+
+const loggerMiddleware = (store: any) => (next: any) => (action: any) => {
+  console.log("store", store);
+  console.log("action", action);
+  next(action);
+};
+
+const middleware = applyMiddleware(thunk, loggerMiddleware);
+const store = createStore(rootReducer, {}, middleware);
+
+console.log("store.getState()", store.getState());
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const render = () =>
+  root.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <App
+          value={store.getState()}
+          onIncrement={() => store.dispatch({ type: "INCREMENT" })}
+          onDecrement={() => store.dispatch({ type: "DECREMENT" })}
+        />
+      </Provider>
+    </React.StrictMode>
+  );
+render();
+
+store.subscribe(render);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
